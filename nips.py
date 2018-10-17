78663fc31ecacc187b7b07212fda27edfcf15f14
LuChengTHU <c-lu15@mails.tsinghua.edu.cn>
2018-10-17 23:05:48 +0800

    Add nips 2016&2017 code

---
+++ nips.py
@@ -0,0 +1,79 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+import random
+import time
+
+year = "2017"
+session = "DL"
+config = {
+    "2016": {
+        "paperItem": "Poster",
+        "DL": ["1", "7"],
+        "RL": ["40", "50"],
+        "BY": ["17", "43", "48"]
+    },
+    "2017": {
+        "paperItem": "Poster",
+        "DL": ["64", "100", "71", "64", "175", "118", "66", "168", "79", "181", "114", "63"],
+        "RL": ["138", "87"],
+        "BY": ["17", "105", "48", "99", "129", "170"]
+    },
+    "2018": {
+        "paperItem": "Poster",
+    }
+}
+
+for session in ["DL", "RL", "BY"]:
+    base_url = "https://nips.cc/Conferences/2017/Schedule?bySubject="
+    for i in config[year][session]:
+        base_url += "&selectedSubject=" + i
+    req = requests.get(base_url)
+    base_html = BeautifulSoup(req.text, 'html.parser')
+
+
+    paperList = []
+    paper_author_list = {}
+    author_institute = {}
+    paper_session_list = {}
+    def isPaper(tag):
+        if tag.has_attr("onclick"):
+            if tag["onclick"].startswith("showDetail"):
+                if config[year]["paperItem"] in tag.div["class"]:
+                    paperList.append("https://nips.cc/Conferences/2018/Schedule?showEvent=" + tag["onclick"][11:-1])
+        return False
+    base_html.find_all(isPaper)
+
+    speakerList = []
+    def isAuthor(tag):
+        if tag.has_attr("onclick"):
+            if tag["onclick"].startswith("showSpeaker"):
+                speakerList.append("https://nips.cc/Conferences/2018/Schedule?showSpeaker=" + tag["onclick"][13:-3])
+        return False
+
+    def isNone(s):
+        if s == '' or s == '\n':
+            return False
+        return True
+
+    for paperUrl in paperList:
+        speakerList.clear()
+        req = requests.get(paperUrl)
+        paper_html = BeautifulSoup(req.text, 'html.parser')
+        paperName = paper_html.find("div", attrs={'class': 'maincardBody'}).text
+        authors = paper_html.find("div", attrs={"class": "maincardFooter"}).text.split(" · ")
+        paper_author_list[paperName] = authors
+        paper_session_list[paperName] = session
+        paper_html.find_all(isAuthor)
+        for authorUrl in speakerList:
+            req = requests.get(authorUrl)
+            author_html = BeautifulSoup(req.text, 'html.parser')
+            tag = author_html.find("div", attrs={"class": "maincard Remark col-sm-12"})
+            author = tag.h3.text
+            institute = tag.h4.text
+            author_institute[author] = institute
+        time.sleep(random.random())
+        print(paperName)
+
+    with open('nips_' + year + '.json', 'w') as file_object:
+        json.dump([paper_author_list, paper_session_list, author_institute], file_object)
