85d902c4bf2cc0a13e219a5d354ac6e2d7a06a73
LuChengTHU <c-lu15@mails.tsinghua.edu.cn>
2018-10-17 21:00:47 +0800

    Add icml.py for 2017&2018

---
+++ icml.py
@@ -0,0 +1,64 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+
+base_url = "https://icml.cc/Conferences/2017/Schedule"
+year = base_url.split("/")[-2]
+req = requests.get(base_url)
+base_html = BeautifulSoup(req.text, 'html.parser')
+
+config = {
+    "2017": {
+        "paperItem": "Talk",
+    },
+    "2018": {
+        "paperItem": "Oral",
+    }
+}
+
+paperList = []
+paper_author_list = {}
+author_institute = {}
+paper_session_list = {}
+def isPaper(tag):
+    if tag.has_attr("onclick"):
+        if tag["onclick"].startswith("showDetail"):
+            if config[year]["paperItem"] in tag.div["class"]:
+                paperList.append("https://icml.cc/Conferences/2018/Schedule?showEvent=" + tag["onclick"][11:-1])
+    return False
+base_html.find_all(isPaper)
+
+speakerList = []
+def isAuthor(tag):
+    if tag.has_attr("onclick"):
+        if tag["onclick"].startswith("showSpeaker"):
+            speakerList.append("https://icml.cc/Conferences/2018/Schedule?showSpeaker=" + tag["onclick"][13:-3])
+    return False
+
+def isNone(s):
+    if s == '' or s == '\n':
+        return False
+    return True
+
+for paperUrl in paperList:
+    speakerList.clear()
+    req = requests.get(paperUrl)
+    paper_html = BeautifulSoup(req.text, 'html.parser')
+    info_div = paper_html.find("div", attrs={'id': 'maincard_' + paperUrl.split("=")[-1]})
+    paperName = info_div.find("div", attrs={"class": "maincardBody"}).text
+    authors = info_div.find("div", attrs={"class": "maincardFooter"}).text.split(" · ")
+    paper_author_list[paperName] = authors
+    sessionName = info_div.find_all("div", attrs={"class": "pull-right maincardHeader maincardType"})[1].a.text
+    sessionName = ' '.join(list(filter(isNone, sessionName.split(" "))))
+    paper_session_list[paperName] = sessionName
+    paper_html.find_all(isAuthor)
+    for authorUrl in speakerList:
+        req = requests.get(authorUrl)
+        author_html = BeautifulSoup(req.text, 'html.parser')
+        tag = author_html.find("div", attrs={"class": "maincard Remark col-sm-12"})
+        author = tag.h3.text
+        institute = tag.h4.text
+        author_institute[author] = institute
+
+with open('icml_' + year + '.json', 'w') as file_object:
+    json.dump([paper_author_list, paper_session_list, author_institute], file_object)
